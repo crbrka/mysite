d575cc61339e1c90234864a6c6837495022d6010
a.tuhvatullin <almir@tuhvatull.in>
2017-06-09 17:57:57 +0300

    Adding new stuff

--- telesite/views.py
+++ telesite/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from .forms import AddPaymentForm
-from .models import *
-from django.http import HttpResponse
+from .models import payment_category
 
 
 def index(request):
-    query_result = payment_category.objects.all()
+    query_result = payment_category.objects.all() #вытягиваем из базы все категории
     AddPaymentHTMLForm = AddPaymentForm(request.POST or None)
     if request.method == "POST":
         if AddPaymentHTMLForm.is_valid():
